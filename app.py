2fde0216117d13d04c2351172258165e55c8bce3
Nihar Karra <niharreddy.k@gmail;.com>
2023-06-03 02:51:07 -0500

    adding error page

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 resume_data = {
-    "name": "John Doe",
+    "name": "Nihar Karra",
     "skills": ["Python", "Java", "JavaScript", "SQL"],
     "certifications": ["AWS Certified Developer", "Microsoft Certified Azure Developer", "New Changes"]
 }
 
-# Route to display skills in HTML
+@app.route('/')
+def home():
+    return f"Welcome to {resume_data['name']}'s Resume API!"
+
 @app.route('/skills', methods=['GET'])
 def display_skills():
     return render_template('skills.html', skills=resume_data["skills"])
 
-# Route to display certifications in HTML
 @app.route('/certifications', methods=['GET'])
 def display_certifications():
     return render_template('certifications.html', certifications=resume_data["certifications"])
 
+# Error handler for routes that are not found
+@app.errorhandler(404)
+def page_not_found(e):
+    # Check the request content type to determine the response format
+    if request.headers.get('Content-Type') == 'application/json':
+        return jsonify(error="404 - Page not found"), 404
+    else:
+        return render_template('error.html', error_message="404 - Page not found"), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
-
