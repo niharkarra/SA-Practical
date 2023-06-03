c83d3b1cae9978860489bc5a4beae579e6c56b06
Nihar Karra <niharreddy.k@gmail;.com>
2023-06-03 03:36:29 -0500

    changes

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 resume_data = {
     "name": "Nihar Karra",
@@ -8,20 +8,20 @@ resume_data = {
     "certifications": ["AWS Certified Developer", "Microsoft Certified Azure Developer", "New Changes"]
 }
 
-@app.route('/')
+@application.route('/')
 def home():
     return f"Welcome to {resume_data['name']}'s Resume API!"
 
-@app.route('/skills', methods=['GET'])
+@application.route('/skills', methods=['GET'])
 def display_skills():
     return render_template('skills.html', skills=resume_data["skills"])
 
-@app.route('/certifications', methods=['GET'])
+@application.route('/certifications', methods=['GET'])
 def display_certifications():
     return render_template('certifications.html', certifications=resume_data["certifications"])
 
 # Error handler for routes that are not found
-@app.errorhandler(404)
+@application.errorhandler(404)
 def page_not_found(e):
     # Check the request content type to determine the response format
     if request.headers.get('Content-Type') == 'application/json':
@@ -30,4 +30,5 @@ def page_not_found(e):
         return render_template('error.html', error_message="404 - Page not found"), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
+    application.debug = True
