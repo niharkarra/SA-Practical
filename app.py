8c8b09c5c4c14fd916d4ce78fef2412d3519a939
Nihar Karra <niharreddy.k@gmail;.com>
2023-06-04 00:29:33 -0500

    chnage2s

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -14,20 +14,16 @@ def home():
 
 @app.route('/skills', methods=['GET'])
 def display_skills():
-    return jsonify(skills=resume_data["skills"])
+    return render_template('skills.html', skills=resume_data["skills"])
 
 @app.route('/certifications', methods=['GET'])
 def display_certifications():
-    return jsonify(certifications=resume_data["certifications"])
+    return render_template('certifications.html', certifications=resume_data["certifications"])
 
 # Error handler for routes that are not found
 @app.errorhandler(404)
 def page_not_found(e):
-    # Check the request content type to determine the response format
-    if request.headers.get('Content-Type') == 'application/json':
-        return jsonify(error="404 - Page not found"), 404
-    else:
-        return render_template('error.html', error_message="404 - Page not found"), 404
+    return "404 - Page not found", 404
 
 if __name__ == '__main__':
     app.run(debug=True)
